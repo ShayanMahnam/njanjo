e0a7a0069fa8a754d3845b4ec2e44a23cc38d699
Shayan Mahnam <shayan.mahnam.uk@gmail.com>
2023-03-01 16:35:23 +0000

    updated

--- src/App.js
+++ src/App.js
@@ -6,9 +6,6 @@ import Footer from './components/Footer';
 import ParticlesBackground from './components/ParticlesBackground';
 import Books from './components/Books';
 import Videos from './components/Videos';
-import Zoom from "react-reveal/Zoom";
-import Reveal from "react-reveal/Reveal";
-
 import './App.css';
 
 function App() {
@@ -16,16 +13,10 @@ function App() {
     <div className="App">
       <ParticlesBackground id="tsparticles" />
       <Header />
-      <Zoom>
         <Hero />
-      </Zoom>
-      <Reveal>
         <About />
         <Books />
-      </Reveal>
-      <Reveal>
         <Videos />
-      </Reveal>
       <Contact />
       <Footer />
     </div>
